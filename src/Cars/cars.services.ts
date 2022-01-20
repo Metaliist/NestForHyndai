853f4523f102c18385f19b9f7bc8a64788c2e53d
Maks <1black2jack3@gmail.com>
2022-01-20 20:52:49 +0300

    Added comments

--- src/Cars/cars.services.ts
+++ src/Cars/cars.services.ts
@@ -6,15 +6,17 @@ import { req as Requests } from './Request'
 @Injectable()
 export class CarService {
     private readonly car: Car = new Car(0, 0, new Date('2022-01-01'), new Date('2022-01-03'));
+    //The method checks the existence of the table, via a query
     private async CheckTable() {
         return client
-            .query(Requests.find(e => e.req == 'Check creation'))
+            .query(Requests.find(e => e.req == 'Check creation')) 
             .catch(async e => {
                 if (e.table == undefined) {
                     await this.CreateTable();
                 }
             })
     }
+    //The method creates a table with a query
     private async CreateTable() {
         client
             .query(Requests.find(e => e.req == 'Create table'))
@@ -23,6 +25,7 @@ export class CarService {
                 throw new Error(e);
             })
     }
+    //The method makes select records from the table and if there is a record, then there is a session, and the car is busy
     private async CheckCar(IDcar: number, DateStart: Date, DateEnd: Date) {
         return await client
             .query(Requests.find(e => e.req == 'Check car'), [IDcar, DateStart, DateEnd])
@@ -37,6 +40,7 @@ export class CarService {
                 throw new Error(e);
             })
     }
+    //The method reserves the car, makes an insert if the car is not occupied and the session satisfies the conditions
     async Rezervecar(IDcar: number, DateStart: Date, DateEnd: Date) {
         if (!this.checkid(IDcar)) {
             return "The identification car is specified more than there is in the park"
@@ -73,6 +77,7 @@ export class CarService {
             return 'Dates are not correctly selected';
         }
     }
+    //Method for insert to table
     private async Rezerved(IDcar: number, DateStart: Date, DateEnd: Date, Price: number) {
         return await client
             .query(Requests.find(e => e.req == 'Rezerve car'), [IDcar, DateStart, DateEnd, Price])
@@ -84,6 +89,7 @@ export class CarService {
                 throw new Error(err);
             })
     }
+    //Method for getting the status of the car, reserved or not
     async Getstatus(IDcar: number, DateStart: Date, DateEnd: Date) {
         if (!this.checkid(IDcar)) {
             return "The identification car is specified more than there is in the park"
@@ -111,6 +117,7 @@ export class CarService {
         }
 
     }
+    //The upper method for checking the status
     private async Getstatuscar(IDcar: number, DateStart: Date, DateEnd: Date) {
         if (!client._connected) {
             client.connect();
@@ -122,11 +129,13 @@ export class CarService {
         });
 
     }
+    //Method for calculating rental days
     private async Calcprise(DateStart: Date, DateEnd: Date) {
         let day = ((+DateEnd - +DateStart) / (60 * 60 * 24 * 1000));
         return await this.SumPrice(day);
 
     }
+    //Method for calculating the rental cost
     private async SumPrice(countday: number) {
         return await client
             .query(Requests.find(e => e.req == 'Sum Price'), [countday])
@@ -138,6 +147,7 @@ export class CarService {
                 throw new Error(err);
             })
     }
+    //Method for converting a date from a string or from a number
     private convertDate(DateStart: Date, DateEnd: Date) {
         if
             (
@@ -150,13 +160,14 @@ export class CarService {
         return { DateStart: DateStart, DateEnd: DateEnd }
     }
     private checkid(idcar: number): boolean {
-        return idcar < 5 && idcar >= 0;
+        return idcar < 5 && idcar >= 0; //Checking the machine ID
     }
+    //Top method for reports
     async orderm(_ordermonth: OrderMonth) {
         if (!client._connected) {
-            client.connect();
+            client.connect(); //Connecting to the database
         }
-        await this.CheckTable()
+        await this.CheckTable() //Table validation method
             .catch(err => { throw new Error(err); });
         let Month;
         if (typeof (_ordermonth.Month) === "string" || typeof (_ordermonth.Month) === 'number') {
@@ -168,6 +179,7 @@ export class CarService {
             return this.ordermonthid(_ordermonth.IDCar, Month);
         }
     }
+    //Method for reporting the average car load by id for the month
     async ordermonthid(idcar: number, month: Date) {
         console.log(month)
         if (!this.checkid(idcar)) {
@@ -192,6 +204,7 @@ export class CarService {
                 throw new Error(err);
             })
     }
+    //Method for reporting the average load of all cars for the month
     async ordermonth(month: Date) {
         let req = Requests.find(e => e.req == 'Order month');
         req.text = req.text.replace('"IDCar" = $2', '1=1');
